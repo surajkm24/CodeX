7095ec7741ce2d28caa171a871311d351493b9e4
Suraj Kumar Mishra <101392142+surajkm24@users.noreply.github.com>
2022-12-26 07:19:14 +0000

    vercel.json created for backend deployment

--- server/src/controllers/code.controllers.js
+++ server/src/controllers/code.controllers.js
@@ -1,5 +1,14 @@
-import {c, cpp, node, python, java} from 'compile-run';
+import { c, cpp, node, python, java } from 'compile-run';
 
+const codeExecutors = {
+    c, cpp, node, python, java
+}
 export const executeCode = async (req, res) => {
+    const lang = req.params.lang;
+    try {
 
+    }
+    catch (error) {
+
+    }
 }
