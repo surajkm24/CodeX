aaeae3b0cfbe59e6b59a091f8df8d065c88c8d5f
Suraj Kumar Mishra <101392142+surajkm24@users.noreply.github.com>
2022-12-27 04:38:08 +0000

    updated

--- server/src/controllers/code.controllers.js
+++ server/src/controllers/code.controllers.js
@@ -1,14 +1,22 @@
-import { c, cpp, node, python, java } from 'compile-run';
+import { execFile } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 
-const codeExecutors = {
-    c, cpp, node, python, java
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const executeCode = async (req, res) => {
     const lang = req.params.lang;
     try {
-
+        execFile('node',[`${__dirname}/test.js`], ((error, stdout, stderr) => {
+            res.send({ error, stdout, stderr,__dirname })
+        }))
+        // fs.readFile(__dirname+'/test.js','utf8',(err,data)=>{
+        //     res.send({data,__dirname})
+        // })
     }
     catch (error) {
-
+        res.send('error')
     }
 }
