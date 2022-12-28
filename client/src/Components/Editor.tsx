7602a470df44e65d8974e9526fc5db0b1a63fccb
surajkm24 <surajkm336796@gmail.com>
2022-12-28 13:52:22 +0530

    styles updated of editor and themes added

--- client/src/Components/Editor.tsx
+++ client/src/Components/Editor.tsx
@@ -1,7 +1,12 @@
-import { Box, Flex, Button } from '@chakra-ui/react';
+import { Box, Flex, Button, useColorMode } from '@chakra-ui/react';
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-twilight";
+import "ace-builds/src-noconflict/theme-tomorrow_night_eighties";
+import "ace-builds/src-noconflict/theme-solarized_dark";
+import "ace-builds/src-noconflict/theme-gruvbox";
+import "ace-builds/src-noconflict/theme-cobalt";
 import "ace-builds/src-noconflict/ext-language_tools";
 import { useState } from 'react';
 
@@ -11,46 +16,61 @@ type props = {
 }
 export const Editor = ({ runCode, loading }: props) => {
     const [code, setCode] = useState<string>('');
+    const { colorMode } = useColorMode();
 
     const writeCode = (value: string) => {
         setCode(value);
     }
 
     return (
-        <Flex w='50%' h='100%' borderRight='1px solid rgba(0,0,0,0.2)' flexDir='column'>
-            <Flex justify={'space-between'} align='center' >
-                <Flex px='20px' bg='#F5F5F5' h='45px' align='center' color='blackAlpha.700'>
+        <Flex w='50%' flexDir='column' borderRadius={'10px'} border='1px'
+            borderColor={colorMode === 'light' ? "rgba(0,0,0,0.1)" : "rgba(255,255,255,0.3)"}>
+            <Flex justify='space-between' p='7px' align='center' borderBottom='1px'
+                bg={colorMode === 'light' ? "white" : "rgb(28,35,51)"}
+                borderColor={colorMode === 'light' ? "rgba(0,0,0,0.2)" : "rgba(255,255,255,0.3)"}
+                borderRadius='10px 10px 0px 0px'>
+                <Button variant='unstyled' fontWeight={400} fontSize='15px'
+                    bg={colorMode === 'light' ? "rgba(0,0,0,0.1)" : 'rgb(43,50,69)'}
+                    h='30px' p='0px 15px'
+                    _hover={{ bg: colorMode === 'light' ? "rgba(0,0,0,0.2)" : "rgba(255,255,255,0.2)" }}
+                >
                     main.js
-                </Flex>
-                <Flex justify={'flex-end'} flexGrow={1} px='20px' py='7px' borderBottom='1px solid rgba(0,0,0,0.2)' borderLeft={'1px solid rgba(0,0,0,0.2)'}>
-                    <Button colorScheme={'blue'} borderRadius='0px' h='30px' fontSize={'14px'}
-                        onClick={() => runCode(code)} isLoading={loading}>
-                        Run
-                    </Button>
-                </Flex>
+                </Button>
+                <Button fontWeight={600} height='30px' borderRadius='3px' fontSize='14px' colorScheme='blue' onClick={() => runCode(code)} isLoading={loading}>
+                    Run
+                </Button>
             </Flex>
-            <Box w='100%' flexGrow={1}>
-                <AceEditor
-                    mode="javascript"
-                    theme="github"
-                    name="blah2"
-                    width='100%'
-                    height='100%'
-                    onChange={writeCode}
-                    fontSize={14}
-                    showPrintMargin={true}
-                    showGutter={true}
-                    highlightActiveLine={true}
-                    value={`${code}`}
-                    setOptions={{
-                        enableBasicAutocompletion: false,
-                        enableLiveAutocompletion: true,
-                        enableSnippets: false,
-                        showLineNumbers: true,
-                        tabSize: 2,
-                    }} />
+            <Flex flexDir={'column'} flexGrow={1}>
+                <Box w='100%' flexGrow={1} >
+                    <AceEditor
+                        mode="javascript"
+                        theme={colorMode === 'light' ? "github" : "cobalt"}
+                        name="code-editor"
+                        width='100%'
+                        height='100%'
+                        wrapEnabled={true}
+                        onChange={writeCode}
+                        fontSize={15}
+                        showPrintMargin={true}
+                        showGutter={true}
+                        highlightActiveLine={true}
+                        value={`${code}`}
+                        setOptions={{
+                            enableBasicAutocompletion: false,
+                            enableLiveAutocompletion: true,
+                            enableSnippets: false,
+                            showLineNumbers: true,
+                            tabSize: 2,
+                        }} />
 
-            </Box>
+                </Box>
+                <Box py='15px'
+                    borderTop='1px'
+                    bg={colorMode === 'light' ? "white" : "rgb(28,35,51)"}
+                    borderColor={colorMode === 'light' ? "rgba(0,0,0,0.2)" : "rgba(255,255,255,0.3)"}
+                    borderRadius='0px 0px 10px 10px'>
+                </Box>
+            </Flex>
         </Flex>
     )
 }
