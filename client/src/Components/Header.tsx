98a119779a0c250d5f8b385c0ee7ff37c883d11a
surajkm24 <surajkm336796@gmail.com>
2022-12-28 12:42:53 +0530

    added dark and light theme button

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,13 +1,23 @@
-import { Box, Text, Image } from '@chakra-ui/react';
+import { Flex, Box, Text, Icon, useColorMode, Button } from '@chakra-ui/react';
 import { Logo } from './Logo';
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 export const Header = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
     return (
-        <Box p='10px'>
-            <Logo />
-            <Text letterSpacing={'0.8px'} fontSize='13px' fontWeight={600} color='blackAlpha.700'>
-                JavaScript Online Compiler
-            </Text>
-        </Box>
+        <Flex justify='space-between' p='10px' align='center'>
+            <Box>
+                <Logo />
+                <Text letterSpacing={'0.8px'} fontSize='13px' fontWeight={600} color={colorMode==='light'?'blackAlpha.700':"wheat"}>
+                    JavaScript Online Compiler
+                </Text>
+            </Box>
+
+            <Button onClick={() => toggleColorMode()}>
+                <Icon as={colorMode === 'light' ? FaMoon : FaSun} cursor='pointer'
+                color={colorMode==='light'?'black':"yellow"} />
+            </Button>
+        </Flex>
     )
 }
